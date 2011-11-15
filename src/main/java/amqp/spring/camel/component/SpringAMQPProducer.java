d5454574f7a69ac7ff455a7047119c64d93de29c
John Ellis <deckerego@gmail.com>
2011-11-15 10:51:18 -0500

    This is an implementation of an async producer, but this won't work out for the short-term. Saving this before I nuke it all.

--- src/main/java/amqp/spring/camel/component/SpringAMQPProducer.java
+++ src/main/java/amqp/spring/camel/component/SpringAMQPProducer.java
@@ -16,15 +16,16 @@
 package amqp.spring.camel.component;
 
 import java.util.Map.Entry;
+import org.apache.camel.AsyncCallback;
 import org.apache.camel.Exchange;
-import org.apache.camel.impl.DefaultProducer;
+import org.apache.camel.impl.DefaultAsyncProducer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.AmqpException;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.MessagePostProcessor;
 
-public class SpringAMQPProducer extends DefaultProducer {
+public class SpringAMQPProducer extends DefaultAsyncProducer {
     private static transient final Logger LOG = LoggerFactory.getLogger(SpringAMQPProducer.class);
     
     protected SpringAMQPEndpoint endpoint;
@@ -37,7 +38,6 @@ public class SpringAMQPProducer extends DefaultProducer {
 
     @Override
     public void process(Exchange exchange) throws Exception {
-        LOG.trace("Sending exchange {}", exchange.getExchangeId());
         Object body = exchange.getIn().getBody();
         
         if(body == null) {
@@ -46,7 +46,35 @@ public class SpringAMQPProducer extends DefaultProducer {
         }
         
         MessagePostProcessor headerProcessor = new HeadersPostProcessor(exchange.getIn());
-        this.endpoint.getAmqpTemplate().convertAndSend(this.endpoint.exchangeName, this.endpoint.routingKey, body, headerProcessor);
+        
+        if(exchange.getPattern().isOutCapable()) {
+            LOG.debug("Synchronous send and request for exchange {}", exchange.getExchangeId());
+            Object response = this.endpoint.getAmqpTemplate().convertSendAndReceive(this.endpoint.exchangeName, this.endpoint.routingKey, body, headerProcessor);
+            exchange.getOut().copyFrom(exchange.getIn());
+            exchange.getOut().setBody(response);
+        } else {
+            LOG.debug("Synchronous send for exchange {}", exchange.getExchangeId());
+            this.endpoint.getAmqpTemplate().convertAndSend(this.endpoint.exchangeName, this.endpoint.routingKey, body, headerProcessor);
+        }
+    }
+
+    @Override
+    public boolean process(Exchange exchange, AsyncCallback callback) {
+        if(exchange.getPattern().isOutCapable()) {
+            LOG.debug("Asynchronous send and request for exchange {}", exchange.getExchangeId());
+            new Thread(new AsyncResponseTask(exchange, callback)).start();
+            return false;
+        } else {
+            try {
+                process(exchange);
+            } catch(Exception e) {
+                exchange.setException(e);
+            } finally {
+                callback.done(true);
+            }
+        
+            return true;
+        }
     }
 
     @Override
@@ -74,6 +102,36 @@ public class SpringAMQPProducer extends DefaultProducer {
                 msg.getMessageProperties().setHeader(headerEntry.getKey(), headerEntry.getValue());
             
             return msg;
+        }
+    }
+    
+    private class AsyncResponseTask implements Runnable {
+        Exchange exchange;
+        AsyncCallback callback;
+        
+        public AsyncResponseTask(Exchange exchange, AsyncCallback callback) {
+            this.exchange = exchange;
+            this.callback = callback;
+        }
+
+        @Override
+        public void run() {
+            MessagePostProcessor headerProcessor = new HeadersPostProcessor(this.exchange.getIn());
+            Object body = this.exchange.getIn().getBody();
+            if(body == null) {
+                LOG.warn("Exchange {} had a null body, creating an empty byte array", this.exchange.getExchangeId());
+                body = new byte[] {};
+            }
+        
+            exchange.getOut().copyFrom(exchange.getIn());
+            try {
+                Object response = endpoint.getAmqpTemplate().convertSendAndReceive(endpoint.getExchangeName(), endpoint.getRoutingKey(), body, headerProcessor);
+                exchange.getOut().setBody(response);
+            } catch (Exception e) {
+                this.exchange.setException(e);
+            } finally {
+                callback.done(false);
+            }
         }
     }
 }
